f00fafa2181458da5fc428449e737b4b39aa31a9
soarflat <soarflat777@gmail.com>
2018-06-05 21:31:46 +0900

    initial commit

---
+++ auth-routes/api/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const router = express.Router();
+const app = express();
+router.use((req, res, next) => {
+  Object.setPrototypeOf(req, app.request);
+  Object.setPrototypeOf(res, app.response);
+  req.res = res;
+  res.req = req;
+  next();
+});
+
+// Add POST - /api/login
+router.post('/login', (req, res) => {
+  if (req.body.username === 'demo' && req.body.password === 'demo') {
+    req.session.authUser = { username: 'demo' };
+    return res.json({ username: 'demo' });
+  }
+  res.status(401).json({ message: 'Bad credentials' });
+});
+
+// Add POST - /api/logout
+router.post('/logout', (req, res) => {
+  delete req.session.authUser;
+  res.json({ ok: true });
+});
+
+export default {
+  path: '/api',
+  handler: router,
+};
